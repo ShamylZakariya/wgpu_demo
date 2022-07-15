9bab8d1a26c3f538e0a89d4b39288cf61aa31747
Shamyl Zakariya <shamyl@gmail.com>
2022-07-15 15:26:40 -0700

    WIP: Compositor draws full screen quad with correct tex coords

---
+++ res/shaders/compositor.wgsl
@@ -0,0 +1,25 @@
+struct VertexOutput {
+    @builtin(position) clip_position: vec4<f32>,
+    @location(0) tex_coord: vec2<f32>,
+};
+
+
+@vertex
+fn compositor_vs_main(
+    @builtin(vertex_index) in_vertex_index: u32,
+) -> VertexOutput {
+    // wgsl doesn't let us index `let` arrays with a variable. So it has to be a `var` local to this function.
+    var fsq_clip_positions:array<vec4<f32>,3> = array<vec4<f32>, 3>(vec4<f32>(-1.0, 1.0, 0.0, 1.0), vec4<f32>(3.0, 1.0, 0.0, 1.0), vec4<f32>(-1.0, -3.0, 0.0, 1.0));
+    var fsq_tex_coords:array<vec2<f32>,3> = array<vec2<f32>, 3>(vec2<f32>(0.0, 0.0), vec2<f32>(2.0, 0.0), vec2<f32>(0.0, 2.0));
+
+    var out: VertexOutput;
+    out.tex_coord = fsq_tex_coords[in_vertex_index];
+    out.clip_position = fsq_clip_positions[in_vertex_index];
+    return out;
+}
+
+@fragment
+fn compositor_fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
+    // for now just dump the tex coord
+    return vec4<f32>(in.tex_coord.x, in.tex_coord.y, 0.0, 1.0);
+}
--- src/lib/compositor.rs
+++ src/lib/compositor.rs
@@ -1,15 +1,78 @@
-use super::{app::AppState, gpu_state};
+use super::{app::AppState, gpu_state, resources};
 
 pub struct Compositor {
     size: winit::dpi::PhysicalSize<u32>,
     time: instant::Duration,
+    render_pipeline: wgpu::RenderPipeline,
 }
 
 impl Compositor {
     pub fn new(gpu_state: &mut gpu_state::GpuState) -> Self {
+        let shader = gpu_state
+            .device
+            .create_shader_module(wgpu::ShaderModuleDescriptor {
+                label: Some("Shader"),
+                source: wgpu::ShaderSource::Wgsl(
+                    resources::load_string_sync("shaders/compositor.wgsl")
+                        .unwrap()
+                        .into(),
+                ),
+            });
+
+        let render_pipeline_layout =
+            gpu_state
+                .device
+                .create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                    label: Some("Render Pipeline Layout"),
+                    bind_group_layouts: &[],
+                    push_constant_ranges: &[],
+                });
+
+        let render_pipeline =
+            gpu_state
+                .device
+                .create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+                    label: Some("Render Pipeline"),
+                    layout: Some(&render_pipeline_layout),
+                    vertex: wgpu::VertexState {
+                        module: &shader,
+                        entry_point: "compositor_vs_main",
+                        buffers: &[],
+                    },
+                    fragment: Some(wgpu::FragmentState {
+                        module: &shader,
+                        entry_point: "compositor_fs_main",
+                        targets: &[Some(wgpu::ColorTargetState {
+                            format: gpu_state.config.format,
+                            blend: Some(wgpu::BlendState {
+                                color: wgpu::BlendComponent::REPLACE,
+                                alpha: wgpu::BlendComponent::REPLACE,
+                            }),
+                            write_mask: wgpu::ColorWrites::ALL,
+                        })],
+                    }),
+                    primitive: wgpu::PrimitiveState {
+                        topology: wgpu::PrimitiveTopology::TriangleList,
+                        strip_index_format: None,
+                        front_face: wgpu::FrontFace::Ccw,
+                        cull_mode: None,
+                        polygon_mode: wgpu::PolygonMode::Fill,
+                        unclipped_depth: false,
+                        conservative: false,
+                    },
+                    depth_stencil: None,
+                    multisample: wgpu::MultisampleState {
+                        count: 1,
+                        mask: !0,
+                        alpha_to_coverage_enabled: false,
+                    },
+                    multiview: None,
+                });
+
         Self {
             size: gpu_state.size(),
             time: instant::Duration::default(),
+            render_pipeline,
         }
     }
 
@@ -46,12 +109,34 @@ impl AppState for Compositor {
     fn render(
         &mut self,
         _gpu_state: &mut gpu_state::GpuState,
-        _encoder: &mut wgpu::CommandEncoder,
-        _output: &wgpu::SurfaceTexture,
+        encoder: &mut wgpu::CommandEncoder,
+        output: &wgpu::SurfaceTexture,
     ) {
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+
+        {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Compositor FSQ Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.1,
+                            b: 0.1,
+                            a: 1.0,
+                        }),
+                        store: true,
+                    },
+                })],
+                depth_stencil_attachment: None,
+            });
+
+            render_pass.set_pipeline(&&self.render_pipeline);
+            render_pass.draw(0..3, 0..1);
+        }
     }
 }
-
-// let view = output
-//     .texture
-//     .create_view(&wgpu::TextureViewDescriptor::default());
